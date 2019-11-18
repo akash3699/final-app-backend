2a8988bc5f3108aad4b8f9097243d6b069625ddc
AKash WaDhawane <akash3699@gmail.com>
2019-11-18 16:46:31 +0530

    200.0.1.155 added

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 function connect() {
     const connection = mysql.createConnection({
-        host: '192.168.44.71',
+        host: '200.0.1.155',
         user: 'root',
         password:'root',
         database: 'app_db',
